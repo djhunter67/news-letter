dae1d1dfd6fc57a96f15201ff0460b21e2cb1ec5
Christerpher Hunter <djhunter67@gmail.com>
2024-02-20 21:15:28 -0500

    add: API server and health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(HttpResponse::Ok)))
+        .bind("0.0.0.0:8000")
+        .unwrap()
+        .workers(1)
+        .run()
+        .await
+        .unwrap();
 }
